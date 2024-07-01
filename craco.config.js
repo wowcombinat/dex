0548a066cc8e3ddeed65aea8fb1b28b210da25be
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-02 01:00:21 +0200

    Create craco.config.js

---
+++ craco.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "os": require.resolve("os-browserify/browser"),
+        "url": require.resolve("url"),
+        "assert": require.resolve("assert"),
+        "zlib": require.resolve("browserify-zlib"),
+        "path": require.resolve("path-browserify")
+      };
+      
+      webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        })
+      );
+
+      return webpackConfig;
+    },
+  },
+};
