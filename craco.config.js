57a1466f841514c8b0a86b040eec10b5bf3e9789
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-02 13:59:43 +0200

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -5,25 +5,20 @@ module.exports = {
     configure: (webpackConfig) => {
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
         "crypto": require.resolve("crypto-browserify"),
         "stream": require.resolve("stream-browserify"),
-        "os": require.resolve("os-browserify/browser"),
-        "url": require.resolve("url"),
         "assert": require.resolve("assert"),
-        "zlib": require.resolve("browserify-zlib"),
-        "path": require.resolve("path-browserify"),
-        "vm": require.resolve("vm-browserify")
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "os": require.resolve("os-browserify"),
+        "url": require.resolve("url")
       };
-      
       webpackConfig.plugins.push(
         new webpack.ProvidePlugin({
           process: 'process/browser',
           Buffer: ['buffer', 'Buffer'],
         })
       );
-
       return webpackConfig;
     },
   },
